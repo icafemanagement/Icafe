289b23edbea557c70f90419bef1606fff84df432
Icafe <icafemanagement@gmail.com>
2020-02-12 14:16:41 +0530

    Backend

--- app/src/main/java/com/example/icafe/MainActivity.java
+++ app/src/main/java/com/example/icafe/MainActivity.java
@@ -3,13 +3,30 @@ package com.example.icafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class
 MainActivity extends AppCompatActivity {
+    private Button admin;
+    private Button vendor;
+    private Button employee;
+    private TextView login;
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        admin = (Button)findViewById(R.id.AdminLogin);
+        vendor = (Button)findViewById(R.id.VendorLogin);
+        employee = (Button)findViewById(R.id.EmployeeLogin);
+        login = (TextView)findViewById(R.id.LoginAs);
+        register = (TextView)findViewById(R.id.Register);
     }
+
+
+
+
 }
