679a90d533c0f75446e39a21cabebebb8ca82acf
Icafe <icafemanagement@gmail.com>
2020-02-18 11:57:04 +0530

    mainfest

--- app/src/main/java/com/example/icafe/employee.java
+++ app/src/main/java/com/example/icafe/employee.java
@@ -1,15 +1,29 @@
 package com.example.icafe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class employee extends AppCompatActivity {
     private EditText _Name3,  _Password3;
     private Button _Login3;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 Window window;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +36,52 @@ Window window;
         window = this.getWindow();
         window.setStatusBarColor(this.getColor(R.color.blue));
 
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user!= null){
+            finish();
+            startActivity(new Intent(employee.this, employee_mainactivity.class));
+        }
+        _Password3.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+
+
+        _Login3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(_Name3.getText().toString(), _Password3.getText().toString());
+            }
+        });
+
+
     }
+
+    private void validate(String Username, String Password){
+
+        progressDialog.setMessage("Verification In Process");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(Username, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss();
+
+                    startActivity(new Intent(employee.this, employee_mainactivity.class));
+                    Toast.makeText(employee.this,"Login Successful", Toast.LENGTH_SHORT).show();
+                    finish();
+                }else{
+                    progressDialog.dismiss();
+                    Toast.makeText(employee.this,"Login Unsuccessful", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/icafe/employee_mainactivity.java
+++ app/src/main/java/com/example/icafe/employee_mainactivity.java
@@ -1,12 +1,20 @@
 package com.example.icafe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class employee_mainactivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
     private Button _recharge, _transaction;
     private ImageButton _imagebutton;
     @Override
@@ -19,4 +27,28 @@ public class employee_mainactivity extends AppCompatActivity {
         _transaction = (Button)findViewById(R.id.transaction);
         _imagebutton = (ImageButton)findViewById(R.id.imagebutton);
     }
+    {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.LogoutMenu:{
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(employee_mainactivity.this , MainActivity.class));
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
