6b1b25eec96d921784f7092585fd023aaa6bcefc
Icafe <icafemanagement@gmail.com>
2020-02-18 17:55:26 +0530

    mainfest

--- app/src/main/java/com/example/icafe/Registeration.java
+++ app/src/main/java/com/example/icafe/Registeration.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,8 +22,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.auth.User;
 
-public class Registeration extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class Registeration extends AppCompatActivity  {
 
     Window window;
     private EditText username;
@@ -28,7 +34,10 @@ public class Registeration extends AppCompatActivity implements AdapterView.OnIt
     private EditText Password;
     private Button register;
     private TextView back;
+    private RadioButton RadioAdmin, RadioVendor, RadioEmployee;
+    private RadioGroup RadioGroup;
     private FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
 
 
 
@@ -42,36 +51,26 @@ public class Registeration extends AppCompatActivity implements AdapterView.OnIt
         Email = (EditText) findViewById(R.id.EmailRegister);
         Password = (EditText) findViewById(R.id.PasswordRegister);
         register = (Button) findViewById(R.id.RegisterButton);
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Role, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
         back = (TextView) findViewById(R.id.BackRegister);
-
-
-
-
+        RadioAdmin = (RadioButton) findViewById(R.id.RadioAdmin);
+        RadioVendor = (RadioButton) findViewById(R.id.RadioVendor);
+        RadioEmployee = (RadioButton) findViewById(R.id.RadioEmployee);
+        RadioGroup = (RadioGroup) findViewById(R.id.RadioGroup);
         window = this.getWindow();
         window.setStatusBarColor(this.getColor(R.color.blue));
 
 
 
 
-
-
-       back.setOnClickListener(new View.OnClickListener(){
-           @Override
-           public void onClick(View view){
-               startActivity(new Intent(Registeration.this, MainActivity.class));
-           }
-       });
-
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        register.setOnClickListener(new View.OnClickListener(){
+        back.setOnClickListener(new View.OnClickListener(){
             @Override
+            public void onClick(View view){
+                startActivity(new Intent(Registeration.this, MainActivity.class));
+            }
+        });
+        firebaseAuth = FirebaseAuth.getInstance();
+       register.setOnClickListener(new View.OnClickListener(){
+           @Override
             public void onClick(View view) {
                 if (validate()) {
 
@@ -80,12 +79,10 @@ public class Registeration extends AppCompatActivity implements AdapterView.OnIt
 
                     firebaseAuth.createUserWithEmailAndPassword(user_email,user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
+                       public void onComplete(@NonNull Task<AuthResult> task) {
 
                             if(task.isSuccessful()){
-                                Toast.makeText(Registeration.this, "Registration Successful", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(Registeration.this, MainActivity.class));
-                            }else{
+                               Toast.makeText(Registeration.this, "Registration Successful", Toast.LENGTH_SHORT).show();                                startActivity(new Intent(Registeration.this, MainActivity.class));
                                 Toast.makeText(Registeration.this, "Registration Unsuccessful", Toast.LENGTH_SHORT).show();
                             }}
                     });
@@ -105,29 +102,18 @@ public class Registeration extends AppCompatActivity implements AdapterView.OnIt
         String password = Password.getText().toString();
         String email = Email.getText().toString();
 
-        if(name.isEmpty() || password.isEmpty() || email.isEmpty()){
+        if(name.isEmpty() || password.isEmpty() && email.isEmpty()){
             Toast.makeText(this, "Please Enter All The Details", Toast.LENGTH_SHORT).show();
 
-        }else{
+       }else{
             result = true;
             finish();
         }
 
         return result;
-    }
 
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),text , Toast.LENGTH_SHORT).show();
+   }
 
 
 
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
---
+++ app/src/main/java/com/example/icafe/Roles.java
@@ -0,0 +1,15 @@
+package com.example.icafe;
+
+public class Roles {
+
+    public String Username, Email , Password , Role;
+
+
+
+    public Roles(String username1, String email1, String password1, String role1) {
+        Username = username1;
+        Email = email1;
+        Password = password1;
+        Role = role1;
+    }
+}
